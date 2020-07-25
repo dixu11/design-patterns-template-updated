de59ac00edbb970c7118343e5e12a872c05ee3a0
Dixu <you.are.imba@gmail.com>
2020-07-26 00:34:16 +0200

    correct mediator package

--- src/main/java/behavioral/mediator/Demo.java
+++ src/main/java/behavioral/mediator/Demo.java
@@ -1,4 +1,4 @@
-package behavioral.mediator.fan_simulation;
+package behavioral.mediator;
 
 public class Demo {
     public static void main(String[] args) {
--- src/main/java/behavioral/mediator/PowerSupplier.java
+++ src/main/java/behavioral/mediator/PowerSupplier.java
@@ -1,4 +1,4 @@
-package behavioral.mediator.fan_simulation;
+package behavioral.mediator;
 
 public class PowerSupplier {
 
