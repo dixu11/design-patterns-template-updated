76d582a77cad71cac0f9714405ae9229943bd9d0
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-09-13 10:54:05 +0200

    strategy gps example

---
+++ src/main/java/behavioral/strategy/example/Gps.java
@@ -0,0 +1,55 @@
+package behavioral.strategy.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Gps {
+
+    private RoadStrategy roadStrategy;
+
+    public int calculateRead() {
+        return roadStrategy.calculateRoad();
+    }
+
+    public void setRoadStrategy(RoadStrategy roadStrategy) {
+        this.roadStrategy = roadStrategy;
+    }
+}
+
+interface RoadStrategy {
+    int calculateRoad();
+}
+
+class WalkRoadStrategy implements RoadStrategy{
+
+    @Override
+    public int calculateRoad() {
+        Random random = new Random();
+        return random.nextInt(10)+5;
+    }
+}
+
+class BikeRoadStrategy implements RoadStrategy{
+
+    @Override
+    public int calculateRoad() {
+        Random random = new Random();
+        return random.nextInt(5)+1;
+    }
+}
+
+class GpsDemo{
+    public static void main(String[] args) {
+        Gps gps = new Gps();
+        gps.setRoadStrategy(new WalkRoadStrategy());
+        System.out.println("Wyliczam trasę");
+        int distance = gps.calculateRead();
+        System.out.println("Trasa: " + distance + "km");
+
+        gps.setRoadStrategy(new BikeRoadStrategy());
+        System.out.println("Wyliczam trasę");
+         distance = gps.calculateRead();
+        System.out.println("Trasa: " + distance + "km");
+
+    }
+}
