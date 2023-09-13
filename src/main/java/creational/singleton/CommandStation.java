28ddb14e473378d34b0b6f40f727e47a6b209972
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-09-13 12:43:24 +0200

    singleton

--- src/main/java/creational/singleton/CommandStation.java
+++ src/main/java/creational/singleton/CommandStation.java
@@ -2,5 +2,23 @@ package creational.singleton;
 
 public class CommandStation {
 
+    private static CommandStation instance = null;//static
+
+
+    private CommandStation() { //prywatny konstroktor!
+        System.out.println("Tworze obiekt");
+    }
+
+    public synchronized static CommandStation getInstance() {
+        if (instance == null) { //implementacja lazy
+            instance = new CommandStation();
+        }
+        return instance;
+    }
+
+    public static void hello() { // wywołanie tej metody powoduje inicjację  statycznych pól a więc w eager implementacji tworzy się obiekt
+        System.out.println("hello world");
+    }
+
 
 }
--- src/main/java/creational/singleton/Demo.java
+++ src/main/java/creational/singleton/Demo.java
@@ -3,9 +3,17 @@ package creational.singleton;
 public class Demo {
 
     public static void main(String[] args) {
+        //Singleton - jeden obiekt na całą aplikację dostępny globalnie
+        //implementacje: eager, lazy, lazy synchronized, enum
+
+
+        CommandStation.hello();
+
+        System.exit(0);
+
         //kod po prawej stronie od '=' należy podmienić
-        CommandStation station = new CommandStation();
-        CommandStation station2 = new CommandStation();
+        CommandStation station = CommandStation.getInstance();
+        CommandStation station2 = CommandStation.getInstance();
 
         //wykonaj implementacje wzorca Singleton, w klasie CommandStation tak, aby
         //poniższy warunek został spełniony
