9b9eb3710f8f0914284bf87cb1a85766c68a17af
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-12-07 12:04:15 +0100

    singleton

--- src/main/java/creational/singleton/CommandStation.java
+++ src/main/java/creational/singleton/CommandStation.java
@@ -1,6 +1,12 @@
 package creational.singleton;
 
 public class CommandStation {
+   private static CommandStation commandStation = new CommandStation();
+    private CommandStation() {
+        System.out.println("Powstaje obiekt");
+    }
 
-
+    public static CommandStation getInstance() {
+        return commandStation;
+    }
 }
--- src/main/java/creational/singleton/Demo.java
+++ src/main/java/creational/singleton/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
 
     public static void main(String[] args) {
         //kod po prawej stronie od '=' należy podmienić
-        CommandStation station = new CommandStation();
-        CommandStation station2 = new CommandStation();
+
+        CommandStation station = CommandStation.getInstance();
+        CommandStation station2 = CommandStation.getInstance();
 
         //wykonaj implementacje wzorca Singleton, w klasie CommandStation tak, aby
         //poniższy warunek został spełniony
