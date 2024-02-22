bbef85be1627464a2448120b1057ad216d996fb5
Daniel Szlicht <daniel.szlicht25@gmail.com>
2024-02-22 14:58:49 +0100

    tanks

---
+++ src/main/java/creational/factoryMethod/tanks/Main.java
@@ -0,0 +1,10 @@
+package creational.factoryMethod.tanks;
+
+class Main {
+    public static void main(String[] args) {
+       /* Tank tankLeft = new TankLeft();
+        Tank tankRight = new TankRight();
+        tankLeft.shoot(); // strzelam w prawo
+        tankRight.shoot(); // strzelam w lewo*/
+    }
+}
